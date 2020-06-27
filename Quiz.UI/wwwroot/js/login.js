3f04d16fae7840697c550736516abacc8b3ca444
Abdullah Kahriman <kahriman.abdullah@gmail.com>
2020-06-27 20:45:20 +0300

    added sign in & sign up pages

---
+++ Quiz.UI/wwwroot/js/login.js
@@ -0,0 +1,24 @@
+﻿//const signUpButton = document.getElementById('signUp');
+//const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+//signUpButton.addEventListener('click', () => {
+//	container.classList.add("right-panel-active");
+//});
+
+//signInButton.addEventListener('click', () => {
+//	container.classList.remove("right-panel-active");
+//});
+
+function setActive() {
+	const path = getPathName();
+	if (path.toLowerCase().indexOf("signup") !== -1) {
+		container.classList.add("right-panel-active");
+	}
+}
+
+setActive();
+
+function getPathName() {
+	return window.location.pathname;
+}
