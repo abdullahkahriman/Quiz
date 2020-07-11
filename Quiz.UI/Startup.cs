f0a4b3545e8a9fd01469225d6ec1cf33051c38e9
Abdullah Kahriman <kahriman.abdullah@gmail.com>
2020-07-12 00:30:04 +0300

    areas added

--- Quiz.UI/Startup.cs
+++ Quiz.UI/Startup.cs
@@ -25,7 +25,7 @@ namespace Quiz.UI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
+
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
@@ -64,6 +64,9 @@ namespace Quiz.UI
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(name: "areas",
+                      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
